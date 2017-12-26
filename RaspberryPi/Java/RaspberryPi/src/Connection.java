4381d026bd7ddd52c7ecf169dfc4fddb5fdc6585
Brendan Manning <BrendanManning19380@gmail.com>
2017-12-26 16:49:52 -0500

    Now using java for RPi

---
+++ RaspberryPi/Java/RaspberryPi/src/Connection.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Pharoah610 on 12/26/17.
+ */
+public class Connection {
+}
---
+++ RaspberryPi/Java/RaspberryPi/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Pharoah610 on 12/26/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
